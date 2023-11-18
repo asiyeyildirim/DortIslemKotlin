80c8dc19b466f09255abeb6231357f828c51b5fd
asiyeyildirim <asiyeeyildirim71@gmail.com>
2023-11-18 18:05:45 +0300

    buton 4 işlem tanımlamaları

--- app/src/main/java/com/kotlinegitim/dortislemkotlin/MainActivity.kt
+++ app/src/main/java/com/kotlinegitim/dortislemkotlin/MainActivity.kt
@@ -3,25 +3,86 @@ package com.kotlinegitim.dortislemkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var sayi1: Int? = null
+    var sayi2: Int? = null
+    var sonuc: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun topla(view: View){
+    fun topla(view: View) {
+
+        sayi1 = editText.text.toString().toIntOrNull()
+        sayi2 = editText2.text.toString().toIntOrNull()
+
+
+
+        if (sayi1 == null || sayi2 == null) {
+            sonuctxt.text = "Sayı girmeyi unutmayınız!"
+
+        } else {
+            sonuc = sayi1!! + sayi2!!
+            sonuctxt.text = "Sonuç: ${sonuc}"
+        }
 
     }
 
 
-    fun cikar(view: View){
+    fun cikar(view: View) {
+
+        sayi1 = editText.text.toString().toIntOrNull()
+        sayi2 = editText2.text.toString().toIntOrNull()
+
+
+
+        if (sayi1 == null || sayi2 == null) {
+            sonuctxt.text = "Sayı girmeyi unutmayınız!"
+
+        } else {
+            sonuc = sayi1!! - sayi2!!
+            sonuctxt.text = "Sonuç: ${sonuc}"
+        }
 
     }
-    fun carp(view: View){
+
+
+    fun carp(view: View) {
+        sayi1 = editText.text.toString().toIntOrNull()
+        sayi2 = editText2.text.toString().toIntOrNull()
+
+
+
+        if (sayi1 == null || sayi2 == null) {
+            sonuctxt.text = "Sayı girmeyi unutmayınız!"
+
+        } else {
+            sonuc = sayi1!! * sayi2!!
+            sonuctxt.text = "Sonuç: ${sonuc}"
+        }
 
     }
-    fun bol(view: View){
 
+
+    fun bol(view: View) {
+
+
+        sayi1 = editText.text.toString().toIntOrNull()
+        sayi2 = editText2.text.toString().toIntOrNull()
+
+
+
+        if (sayi1 == null || sayi2 == null) {
+            sonuctxt.text = "Sayı girmeyi unutmayınız!"
+
+        } else {
+            sonuc = sayi1!! /  sayi2!!
+            sonuctxt.text = "Sonuç: ${sonuc}"
+        }
     }
 }
